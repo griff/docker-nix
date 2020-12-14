b418c669fc06288154f137c85b675df0303ba554
Brian Olsen <brian@maven-group.org>
2020-12-14 22:15:36 +0100

    Add automatic update action

---
+++ update-version.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+NIX_VERSION="$(curl -L https://nixos.org/nix/install | sed -nE 's/.*Nix ([0-9]+\.[0-9]+\.[0-9]+) bindary.*/\1/p')"
+if [ -z "$NIX_VERSION" ]; then
+  echo "No version found. Script needs to be updated..."
+  exit 1
+fi
+
+echo "NIX_VERSION=\"$NIX_VERSION\"" > version.sh
