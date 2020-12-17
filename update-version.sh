019fd1873b07770202e6e2f2aa487fb591dae96a
Brian Olsen <brian@maven-group.org>
2020-12-17 14:42:37 +0100

    Fix spelling error in update-version.sh

--- update-version.sh
+++ update-version.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-NIX_VERSION="$(curl -L https://nixos.org/nix/install | sed -nE 's/.*Nix ([0-9]+\.[0-9]+\.[0-9]+) bindary.*/\1/p')"
+NIX_VERSION="$(curl -L https://nixos.org/nix/install | sed -nE 's/.*Nix ([0-9]+\.[0-9]+\.[0-9]+) binary.*/\1/p')"
 if [ -z "$NIX_VERSION" ]; then
   echo "No version found. Script needs to be updated..."
   exit 1
